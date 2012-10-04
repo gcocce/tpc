6cd3cf1b24569cedf50d464b5a5ac6858876aece
gcocce <gastoncocce@hotmail.com>
2012-10-04 19:15:33 -0300

    Prueba de actualización

--- tpc/src/tpc.cpp
+++ tpc/src/tpc.cpp
@@ -1,15 +1,18 @@
 //============================================================================
 // Name        : tpc.cpp
-// Author      : Gk
 // Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Proceso principal
 //============================================================================
 
 #include <iostream>
 using namespace std;
 
 int main() {
+
+
+
+
+
 	cout << "Trabajo practico de concurrentes." << endl;
 	return 0;
 }
