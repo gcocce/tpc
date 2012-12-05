a46f51d8689f4be699af337e8e3958dca3573d46
gcocce <gastoncocce@hotmail.com>
2012-12-05 04:39:55 -0300

    correccion de bug de finalizacion en el Gestor

--- tpc2/src/GestorConsult.cpp
+++ tpc2/src/GestorConsult.cpp
@@ -88,136 +88,140 @@ void GestorConsulta::run(){
 			this->estado=1;
 			continue;
 		}
-		{
-		std::stringstream stringStream;
-		stringStream << "GestorConsulta, mensaje recibido: from-" << buffer.id << " tipo-" << buffer.type << " estacionamiento-" << buffer.value;
-		string copyOfStr = stringStream.str();
-		this->log->debug(copyOfStr.c_str());
+		// Comprobamos que no se le indicó finalizar al programa
+		if (!this->getFinalizar()){
+			{
+			std::stringstream stringStream;
+			stringStream << "GestorConsulta, mensaje recibido: from-" << buffer.id << " tipo-" << buffer.type << " estacionamiento-" << buffer.value;
+			string copyOfStr = stringStream.str();
+			this->log->debug(copyOfStr.c_str());
+			}
+
+			bloquearSigint();
+			switch (buffer.type) {
+				case 0: //Cantidad estacionamientos
+					{
+						MsgF msgf;
+						msgf.setConsulta(MsgF::cantidadEstacionamientos);
+						msgf.setEstacionamiento(0);
+
+						string str=msgf.toString();
+						{
+						std::stringstream stringStream;
+						stringStream << "GestorConsulta, consulta enviada: " << str;
+						string copyOfStr = stringStream.str();
+						this->log->debug(copyOfStr.c_str());
+						}
+
+						MsgFST st=msgf.toStruct();
+						// Se envia la consulta al administrador general
+						this->cpipe->escribir((void*)&st,sizeof(st));
+
+						/* Esperar respuesta del AdministradorGeneral */
+						MsgFST st2;
+
+						this->canalEAdmin.waitRead();
+						st2=this->canalEAdmin.leer();
+
+						MsgF msgr(st2);
+						{
+						std::stringstream stringStream;
+						stringStream << "GestorConsulta, respuesta recibida: " << msgr.toString();
+						string copyOfStr = stringStream.str();
+						this->log->debug(copyOfStr.c_str());
+						}
+
+						buffer.mtype=buffer.id;
+						buffer.value=msgr.getEstacionamiento();
+						queue.escribir(buffer);
+					}
+					break;
+				case 1: //Monto recaudado
+					/* Consultar al AdministradorGeneral */
+					{
+						MsgF msgf;
+						msgf.setConsulta(MsgF::estadoEstacionamiento);
+						msgf.setEstacionamiento(buffer.value);
+
+						string str=msgf.toString();
+						{
+						std::stringstream stringStream;
+						stringStream << "GestorConsulta: Est " << (int)buffer.value << ": consulta enviada: " << str;
+						string copyOfStr = stringStream.str();
+						this->log->debug(copyOfStr.c_str());
+						}
+
+						MsgFST st=msgf.toStruct();
+
+						// Se envia la consulta al administrador general
+						this->cpipe->escribir((void*)&st,sizeof(st));
+
+
+						/* Esperar respuesta del AdministradorGeneral */
+						MsgFST st2;
+
+						this->canalEAdmin.waitRead();
+						st2=this->canalEAdmin.leer();
+
+						MsgF msgr(st2);
+						{
+						std::stringstream stringStream;
+						stringStream << "GestorConsulta: Est " << (int)buffer.value << ": respuesta recibida: " << msgr.toString();
+						string copyOfStr = stringStream.str();
+						this->log->debug(copyOfStr.c_str());
+						}
+
+						buffer.mtype=buffer.id;
+						buffer.value=msgr.getMonto();
+						queue.escribir(buffer);
+					}
+					break;
+				case 2: //Autos estacionados
+					/* Consultar al AdministradorGeneral */
+					{
+						MsgF msgf;
+						msgf.setConsulta(MsgF::estadoEstacionamiento);
+						msgf.setEstacionamiento(buffer.value);
+
+						string str=msgf.toString();
+						{
+						std::stringstream stringStream;
+						stringStream << "GestorConsulta: Est " << (int)buffer.value << ": consulta enviada: " << str;
+						string copyOfStr = stringStream.str();
+						this->log->debug(copyOfStr.c_str());
+						}
+
+						MsgFST st=msgf.toStruct();
+
+						// Se envia la consulta al administrador general
+						this->cpipe->escribir((void*)&st,sizeof(st));
+
+
+						/* Esperar respuesta del AdministradorGeneral */
+						MsgFST st2;
+
+						this->canalEAdmin.waitRead();
+						st2=this->canalEAdmin.leer();
+
+						MsgF msgr(st2);
+						{
+						std::stringstream stringStream;
+						stringStream << "GestorConsulta: Est " << (int)buffer.value << ": respuesta recibida: " << msgr.toString();
+						string copyOfStr = stringStream.str();
+						this->log->debug(copyOfStr.c_str());
+						}
+
+						buffer.mtype=buffer.id;
+						buffer.value=msgr.getLugar();
+						queue.escribir(buffer);
+					}
+					break;
+				default:
+					this->log->debug("Consulta extraña.");
+				break;
+			}
+			desbloquearSigint();
 		}
-		bloquearSigint();
-		switch (buffer.type) {
-			case 0: //Cantidad estacionamientos
-				{
-					MsgF msgf;
-					msgf.setConsulta(MsgF::cantidadEstacionamientos);
-					msgf.setEstacionamiento(0);
-
-					string str=msgf.toString();
-					{
-					std::stringstream stringStream;
-					stringStream << "GestorConsulta, consulta enviada: " << str;
-					string copyOfStr = stringStream.str();
-					this->log->debug(copyOfStr.c_str());
-					}
-
-					MsgFST st=msgf.toStruct();
-					// Se envia la consulta al administrador general
-					this->cpipe->escribir((void*)&st,sizeof(st));
-
-					/* Esperar respuesta del AdministradorGeneral */
-					MsgFST st2;
-
-					this->canalEAdmin.waitRead();
-					st2=this->canalEAdmin.leer();
-
-					MsgF msgr(st2);
-					{
-					std::stringstream stringStream;
-					stringStream << "GestorConsulta, respuesta recibida: " << msgr.toString();
-					string copyOfStr = stringStream.str();
-					this->log->debug(copyOfStr.c_str());
-					}
-
-					buffer.mtype=buffer.id;
-					buffer.value=msgr.getEstacionamiento();
-					queue.escribir(buffer);
-				}
-				break;
-			case 1: //Monto recaudado
-				/* Consultar al AdministradorGeneral */
-				{
-					MsgF msgf;
-					msgf.setConsulta(MsgF::estadoEstacionamiento);
-					msgf.setEstacionamiento(buffer.value);
-
-					string str=msgf.toString();
-					{
-					std::stringstream stringStream;
-					stringStream << "GestorConsulta: Est " << (int)buffer.value << ": consulta enviada: " << str;
-					string copyOfStr = stringStream.str();
-					this->log->debug(copyOfStr.c_str());
-					}
-
-					MsgFST st=msgf.toStruct();
-
-					// Se envia la consulta al administrador general
-					this->cpipe->escribir((void*)&st,sizeof(st));
-
-
-					/* Esperar respuesta del AdministradorGeneral */
-					MsgFST st2;
-
-					this->canalEAdmin.waitRead();
-					st2=this->canalEAdmin.leer();
-
-					MsgF msgr(st2);
-					{
-					std::stringstream stringStream;
-					stringStream << "GestorConsulta: Est " << (int)buffer.value << ": respuesta recibida: " << msgr.toString();
-					string copyOfStr = stringStream.str();
-					this->log->debug(copyOfStr.c_str());
-					}
-
-					buffer.mtype=buffer.id;
-					buffer.value=msgr.getMonto();
-					queue.escribir(buffer);
-				}
-				break;
-			case 2: //Autos estacionados
-				/* Consultar al AdministradorGeneral */
-				{
-					MsgF msgf;
-					msgf.setConsulta(MsgF::estadoEstacionamiento);
-					msgf.setEstacionamiento(buffer.value);
-
-					string str=msgf.toString();
-					{
-					std::stringstream stringStream;
-					stringStream << "GestorConsulta: Est " << (int)buffer.value << ": consulta enviada: " << str;
-					string copyOfStr = stringStream.str();
-					this->log->debug(copyOfStr.c_str());
-					}
-
-					MsgFST st=msgf.toStruct();
-
-					// Se envia la consulta al administrador general
-					this->cpipe->escribir((void*)&st,sizeof(st));
-
-
-					/* Esperar respuesta del AdministradorGeneral */
-					MsgFST st2;
-
-					this->canalEAdmin.waitRead();
-					st2=this->canalEAdmin.leer();
-
-					MsgF msgr(st2);
-					{
-					std::stringstream stringStream;
-					stringStream << "GestorConsulta: Est " << (int)buffer.value << ": respuesta recibida: " << msgr.toString();
-					string copyOfStr = stringStream.str();
-					this->log->debug(copyOfStr.c_str());
-					}
-
-					buffer.mtype=buffer.id;
-					buffer.value=msgr.getLugar();
-					queue.escribir(buffer);
-				}
-				break;
-			default:
-				this->log->debug("Consulta extraña.");
-			break;
-		}
-		desbloquearSigint();
 	}
 	log->debug("GestorConsulta: finaliza el metodo run.");
 }
